28ccdebe41f5698a52676027e01125c5db6a52ca
Triple Toxic <khoango2007khoa@gmail.com>
2022-11-04 15:24:59 -0400

    ppdkf

--- src/stuff/logic/VFunc.java
+++ src/stuff/logic/VFunc.java
@@ -10,24 +10,9 @@ public enum VFunc {
     public static final VFunc[] all = values();
 
     public final String symbol;
-    public final Boolean scalar, cross;
 
-    VFunc(String symbol, NormalVector opIn){
+    VFunc(String symbol){
         this.symbol = symbol;
-        this.op1 = null;
-        this.op2 = opIn;
-        this.op3 = null;
-        this.scalar = false;
-        this.cross = false;
-    }
-
-    VFunc(String symbol, Boolean isScalar, NormalScalar opIn){
-        this.symbol = symbol;
-        this.op1 = opIn;
-        this.op2 = null;
-        this.op3 = null;
-        this.scalar = isScalar;
-        this.cross = false;
     }
 
     public String getSymbol() {
