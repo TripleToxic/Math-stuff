f4e3ce84aa3b900a65da8838ccb1aa09a278dd43
TripleToxic <khoango2007khoa@gmail.com>
2023-05-15 11:29:53 -0400

    a

--- src/stuff/util/Function.java
+++ src/stuff/util/Function.java
@@ -1,31 +1,49 @@
 package stuff.util;
 
-public class Function{
-    public Function f1, f2;
-    public DVar var1, var2;
-    public boolean isUnary;
+import mindustry.logic.LExecutor;
 
-    /**
-        A double variable with name and value
-    */
-    public static class DVar{
+public abstract class Function{
+    public static final Function[] New = {new Add(), new Sub()};
+
+    public Function f1, f2;
+    public String inputName;
+
+    public static Function process(LExecutor exec, String in){
+        StringBuilder out = new StringBuilder();
+        String buffer = "";
+
+        buffer = in.substring(0, in.indexOf("("));
+
+        return New[0];
+    }
+
+    public abstract double evaluate(double x);
+
+    public static class DVar extends Function{
         public String name;
         public double value = 0;
-
+    
         public DVar(String name){
             this.name = name;
         }
-    }
-
-    public static class Add extends Function implements BinaryFunctionStuff{
 
         @Override
-        public double evaluate(Function F){
-            return 0;
+        public double evaluate(double x) {
+            return name.equals(inputName) ? x : value;
         }
     }
 
-    public interface BinaryFunctionStuff{
-        double evaluate(Function F);
+    public static class Add extends Function{
+        @Override
+        public double evaluate(double x){
+           return f1.evaluate(x) + f2.evaluate(x);
+        }
+    }
+
+    public static class Sub extends Function{
+        @Override
+        public double evaluate(double x){
+           return f1.evaluate(x) - f2.evaluate(x);
+        }
     }
 }
